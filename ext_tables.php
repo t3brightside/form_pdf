af22d7185bbcf40d456240dcba5e3c86d0fd595d
Tanel Põld <tanel@brightside.ee>
2025-01-23 23:39:30 +0200

    [TASK] clean up for v12

--- ext_tables.php
+++ ext_tables.php
@@ -6,13 +6,3 @@ if(!class_exists('\Mpdf\Mpdf')){
         . 'Resources/Private/PHP/autoload.php';
     require_once($composerAutoloadFile);
 }
-
-call_user_func(
-    function () {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_formpdf_domain_model_pdftemplate', 'EXT:form_pdf/Resources/Private/Language/locallang_csh_tx_formpdf_domain_model_pdftemplate.xlf');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_formpdf_domain_model_pdftemplate');
-
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_formpdf_domain_model_htmltemplate', 'EXT:form_pdf/Resources/Private/Language/locallang_csh_tx_formpdf_domain_model_htmltemplate.xlf');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_formpdf_domain_model_htmltemplate');
-    }
-);
